664df1473dba3b2995dba4e24a018407d40a1dc6
Corne Siebeling <2078538@talnet.nl>
2020-09-23 10:01:34 +0200

    taak 2

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,13 @@
+<?php
+    $leeftijd = 0;
+
+    while($leeftijd <= 17) {
+        echo "Ik ben $leeftijd dus ik mag nog niet stemmen <br>";
+        $leeftijd++;
+      }
+
+      while($leeftijd >= 18) {
+        echo "ik ben $leeftijd dus ik heb stemrecht <br>";
+        $leeftijd++;
+      }
+?>
---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,12 @@
+<?php
+    $leeftijd = 0;
+
+
+    for ($leeftijd=0; $leeftijd <= 17; $leeftijd++) { 
+        echo "ik ben  $leeftijd , dus ik mag nog niet stemmen <br>"
+    }
+
+    for ($leeftijd=0; $leeftijd <= 18; $leeftijd++) { 
+        echo "ik ben  $leeftijd , dus ik heb stemrecht <br>"
+    }
+?>
